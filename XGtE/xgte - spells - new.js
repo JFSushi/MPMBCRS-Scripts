38aef887c7eaac1bbd76e2a2ed5c298530d454d9
JFSushi <jallefrido@gmail.com>
2017-11-20 13:41:51 +1100

    XGtE New Spells
    Descriptions still missing

---
+++ XGtE/xgte - spells - new.js
@@ -0,0 +1,619 @@
+//Written by JFSushi
+//Includes all new spells introduced in Xanathar's Guide to Everything
+
+
+//Incomplete - destriptions missing
+
+SpellsList["abi-dalzim's horrid wilting"] = {
+	name : "Abi-Dalzim's Horrid Wilting",
+	classes : ["sorcerer","wizard"],
+	source : ["X", 150],
+	level : 8,
+	school : "Necro",
+	time : "1 a",
+	range : "150 ft",
+	components : "V,S,M",
+	compMaterial : "A bit of sponge",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["absorb elements"] = {
+	name : "Absorb Elements",
+	classes : ["druid","ranger","sorcerer","wizard"],
+	source : ["X", 150],
+	level : 1,
+	school : "Abjur",
+	time : "1 rea",
+	range : "Self",
+	components : "S",
+	duration : "1 rnd",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["catnap"] = {
+	name : "Catnap",
+	classes : ["bard","sorcerer","wizard"],
+	source : ["X", 151],
+	level : 3,
+	school : "Ench",
+	time : "1 a",
+	range : "30 ft",
+	components : "S,M",
+	compMaterial : "A pinch of sand",
+	duration : "10 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["charm monster"] = {
+	name : "Charm Monster",
+	classes : ["bard","druid","sorcerer","warlock","wizard"],
+	source : ["X", 151],
+	level : 4,
+	school : "Ench",
+	time : "1 a",
+	range : "30 ft",
+	components : "V,S",
+	duration : "1 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["create homunculus"] = {
+	name : "Create Homunculus",
+	classes : ["wizard"],
+	source : ["X", 152],
+	level : 6,
+	school : "Trans",
+	time : "1 h",
+	range : "Touch",
+	components : "V,S,M\u2020",
+	compMaterial : "Clay, ash and mandrake root, all of which are consumed, and a jewel-encrusted dagger worth at least 1000 gp",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["crown of stars"] = {
+	name : "Crown of Stars",
+	classes : ["sorcerer","warlock","wizard"],
+	source : ["X", 152],
+	level : 7,
+	school : "Evoc",
+	time : "1 a",
+	range : "Self",
+	components : "V,S",
+	duration : "1 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["danse macabre"] = {
+	name : "Danse Macabre",
+	classes : ["warlock","wizard"],
+	source : ["X", 153],
+	level : 5,
+	school : "Necro",
+	time : "1 a",
+	range : "60 ft",
+	components : "V,S",
+	duration : "Conc, 1 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["dawn"] = {
+	name : "Dawn",
+	classes : ["cleric","wizard"],
+	source : ["X", 153],
+	level : 5,
+	school : "Evoc",
+	time : "1 a",
+	range : "60 ft",
+	components : "V,S,M",
+	compMaterial : "A sunburst pendant worth at least 100gp",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["dragon's breath"] = {
+	name : "Dragon's Breath",
+	classes : ["sorcerer","wizard"],
+	source : ["X", 154],
+	level : 2,
+	school : "Trans",
+	time : "1 bns",
+	range : "Touch",
+	components : "V,S,M",
+	compMaterial : "A hot pepper",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["druid grove"] = {
+	name : "Druid Grove",
+	classes : ["druid"],
+	source : ["X", 154],
+	level : 6,
+	school : "Abjur",
+	time : "10 min",
+	range : "Touch",
+	components : "V,S,M\u2020",
+	compMaterial : "-",
+	duration : "24 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["enemies abound"] = {
+	name : "Enemies Abound",
+	classes : ["bard","sorcerer","warlock","wizard"],
+	source : ["X", 155],
+	level : 3,
+	school : "Ench",
+	time : "1 a",
+	range : "120 ft",
+	components : "V,S",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["enervation"] = {
+	name : "Enervation",
+	classes : ["sorcerer","warlock","wizard"],
+	source : ["X", 155],
+	level : 5,
+	school : "Necro",
+	time : "1 a",
+	range : "60 ft",
+	components : "V,S",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["far step"] = {
+	name : "Far Step",
+	classes : ["sorcerer","warlock","wizard"],
+	source : ["X", 155],
+	level : 5,
+	school : "Conj",
+	time : "1 bns",
+	range : "Self",
+	components : "V",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["find greater steed"] = {
+	name : "Find Greater Steed",
+	classes : ["paladin"],
+	source : ["X", 156],
+	level : 4,
+	school : "Conj",
+	time : "10 min",
+	range : "30 ft",
+	components : "V,S",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["guardian of nature"] = {
+	name : "Guardian of Nature",
+	classes : ["druid","ranger"],
+	source : ["X", 157],
+	level : 4,
+	school : "Trans",
+	time : "1 bns",
+	range : "Self",
+	components : "V",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["healing spirit"] = {
+	name : "Healing Spirit",
+	classes : ["druid","ranger"],
+	source : ["X", 157],
+	level : 2,
+	school : "Trans",
+	time : "1 bns",
+	range : "60 ft",
+	components : "V,S",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["holy weapon"] = {
+	name : "Holy Weapon",
+	classes : ["cleric","paladin"],
+	source : ["X", 157],
+	level : 5,
+	school : "Evoc",
+	time : "1 bns",
+	range : "Touch",
+	components : "V,S",
+	duration : "Conc, 1 h",
+	save: "Con",
+	description : "Wpn deal +2d8 Radiant dmg, create 30ft/30ft light, dispel 4d8 Radiant in 30ft +blind 1m; see desc",
+	descriptionFull : "You imbue a weapon you touch with holy power. Until the spell ends, the weapon emits a bright light in a 30-foot radius and dim light for an additional 30 feet. In addition, weapon attacks made with it deal an extra 2d8 radiant damage on a hit. If the weapon isn't already a magic weapon, it becomes one for the duration." + "\n   " + "As a bonus action on your turn, you can dismiss this spell and cause the weapon to emit a burst of radiance. Each creature of your choice that you can see within 30 feet of you must make a Constitution saving throw. On a failed save, a creature takes 4d8 radiant damage, and it is blinded for 1 minute. On a successful save, a creature takes half as much damage and isn't blinded. At the end of each of it turns, a blinded creature can make a Constitution saving throw, endind the effect on itself on a success."
+};
+SpellsList["illusory dragon"] = {
+	name : "Illusory Dragon",
+	classes : ["wizard"],
+	source : ["X", 157],
+	level : 8,
+	school : "Illus",
+	time : "1 a",
+	range : "120 ft",
+	components : "S",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["infernal calling"] = {
+	name : "Infernal Calling",
+	classes : ["warlock","wizard"],
+	source : ["X", 158],
+	level : 5,
+	school : "Conj",
+	time : "1 m",
+	range : "90 ft",
+	components : "V,S,M",
+	compMaterial : "A ruby worth at least 999gp",
+	duration : "Conc, 1 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["life transference"] = {
+	name : "Life Transference",
+	classes : ["cleric","wizard"],
+	source : ["X", 160],
+	level : 3,
+	school : "Necro",
+	time : "1 a",
+	range : "30 ft",
+	components : "V,S",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["maddening darkness"] = {
+	name : "Maddening Darkness",
+	classes : ["warlock","wizard"],
+	source : ["X", 160],
+	level : 8,
+	school : "Evoc",
+	time : "1 a",
+	range : "150 ft",
+	components : "V,M",
+	compMaterial : "A drop of pitch mixed with a drop of mercury",
+	duration : "Conc, 10 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["mass polymorph"] = {
+	name : "Mass Polymorph",
+	classes : ["bard","sorcerer","wizard"],
+	source : ["X", 160],
+	level : 9,
+	school : "Trans",
+	time : "1 a",
+	range : "120 ft",
+	components : "V,S,M",
+	compMaterial : "A caterpillar cocoon",
+	duration : "Conc, 1 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["mental prison"] = {
+	name : "Mental Prison",
+	classes : ["sorcerer","warlock","wizard"],
+	source : ["X", 161],
+	level : 6,
+	school : "Illus",
+	time : "1 a",
+	range : "60 ft",
+	components : "S",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["mighty fortress"] = {
+	name : "Mighty Fortress",
+	classes : ["wizard"],
+	source : ["X", 161],
+	level : 8,
+	school : "Conj",
+	time : "1 min",
+	range : "1 mile",
+	components : "V,S,M\2020",
+	compMaterial : "A diamond worth at least 500gp, which the spell consumes.",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["mind spike"] = {
+	name : "Mind Spike",
+	classes : ["sorcerer","warlock","wizard"],
+	source : ["X", 162],
+	level : 2,
+	school : "Div",
+	time : "1 a",
+	range : "60 ft",
+	components : "S",
+	duration : "Conc, 1 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["negative energy flood"] = {
+	name : "Negative Energy Flood",
+	classes : ["warlock","wizard"],
+	source : ["X", 163],
+	level : 5,
+	school : "Necro",
+	time : "1 a",
+	range : "60 ft",
+	components : "V,M",
+	compMaterial : "A broken bone and a square of black silk.",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["power word pain"] = {
+	name : "Power Word Pain",
+	classes : ["sorcerer","warlock","wizard"],
+	source : ["X", 163],
+	level : 7,
+	school : "Ench",
+	time : "1 a",
+	range : "60 ft",
+	components : "V",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["psychic scream"] = {
+	name : "Psychic Scream",
+	classes : ["bard","sorcerer","warlock","wizard"],
+	source : ["X", 163],
+	level : 9,
+	school : "Ench",
+	time : "1 a",
+	range : "90 ft",
+	components : "S",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["scatter"] = {
+	name : "Scatter",
+	classes : ["sorcerer","warlock","wizard"],
+	source : ["X", 164],
+	level : 6,
+	school : "Conj",
+	time : "1 a",
+	range : "30 ft",
+	components : "V",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["shadow blade"] = {
+	name : "Shadow Blade",
+	classes : ["sorcerer","warlock","wizard"],
+	source : ["X", 164],
+	level : 2,
+	school : "Illus",
+	time : "1 bns",
+	range : "Self",
+	components : "V,S",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["shadow of moil"] = {
+	name : "Shadow of Moil",
+	classes : ["warlock"],
+	source : ["X", 164],
+	level : 4,
+	school : "Necro",
+	time : "1 a",
+	range : "Self",
+	components : "V,S,M",
+	compMaterial : "An undead eyeball encased in a gem worth at least 150gp.",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["sickening radiance"] = {
+	name : "Sickening Radiance",
+	classes : ["sorcerer","warlock","wizard"],
+	source : ["X", 164],
+	level : 4,
+	school : "Evoc",
+	time : "1 a",
+	range : "120 ft",
+	components : "V,S",
+	duration : "Conc, 10 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["skill empowerment"] = {
+	name : "Skill Empowerment",
+	classes : ["bard","sorcerer","wizard"],
+	source : ["X", 165],
+	level : 5,
+	school : "Trans",
+	time : "1 a",
+	range : "Touch",
+	components : "V,S",
+	duration : "Conc, 1 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["soul cage"] = {
+	name : "Soul Cage",
+	classes : ["warlock","wizard"],
+	source : ["X", 165],
+	level : 6,
+	school : "Necro",
+	time : "1 rea",
+	range : "60 ft",
+	components : "V,S,M",
+	compMaterial : "A tiny silver cage worth 100gp.",
+	duration : "8 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["steel wind strike"] = {
+	name : "Steel Wind Strike",
+	classes : ["ranger","wizard"],
+	source : ["X", 166],
+	level : 5,
+	school : "Conj",
+	time : "1 a",
+	range : "30 ft",
+	components : "S,M",
+	compMaterial : "A melee weapon worth at least 1 sp.",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["summon greater demon"] = {
+	name : "Summon Greater Demon",
+	classes : ["warlock","wizard"],
+	source : ["X", 166],
+	level : 4,
+	school : "Conj",
+	time : "1 a",
+	range : "60 ft",
+	components : "V,S,M",
+	compMaterial : "A vial of blood from a humanoid killed within the past 24 hours.",
+	duration : "Conc, 1 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["summon lesser demons"] = {
+	name : "Summon Lesser Demons",
+	classes : ["warlock","wizard"],
+	source : ["X", 167],
+	level : 3,
+	school : "Conj",
+	time : "1 a",
+	range : "60 ft",
+	components : "V,S,M",
+	compMaterial : "A vial of blood from a humanoid killed within the past 24 hours.",
+	duration : "Conc, 1 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["synaptic static"] = {
+	name : "Synaptic Static",
+	classes : ["bard","sorcerer","warlock","wizard"],
+	source : ["X", 167],
+	level : 5,
+	school : "Ench",
+	time : "1 a",
+	range : "120 ft",
+	components : "V,S",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["temple of the gods"] = {
+	name : "Temple of the Gods",
+	classes : ["cleric"],
+	source : ["X", 167],
+	level : 7,
+	school : "Conj",
+	time : "1 h",
+	range : "120 ft",
+	components : "V,S,M",
+	compMaterial : "A holy symbol worth at least 5gp.",
+	duration : "24 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["tenser's transformation"] = {
+	name : "Tenser's Transformation",
+	classes : ["wizard"],
+	source : ["X", 168],
+	level : 6,
+	school : "Trans",
+	time : "1 a",
+	range : "Self",
+	components : "V,S,M",
+	compMaterial : "A few hairs from a bull.",
+	duration : "Conc, 10 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["thunder step"] = {
+	name : "Thunder Step",
+	classes : ["warlock","wizard"],
+	source : ["X", 168],
+	level : 3,
+	school : "Conj",
+	time : "1 a",
+	range : "90 ft",
+	components : "V",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["tiny servant"] = {
+	name : "Tiny Servant",
+	classes : ["wizard"],
+	source : ["X", 168],
+	level : 3,
+	school : "Trans",
+	time : "1 min",
+	range : "Touch",
+	components : "V,S",
+	duration : "8 h",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["wall of light"] = {
+	name : "Wall of Light",
+	classes : ["sorcerer","warlock","wizard"],
+	source : ["X", 170],
+	level : 5,
+	school : "Evoc",
+	time : "1 a",
+	range : "120 ft",
+	components : "V,S,M",
+	compMaterial : "A hand mirror.",
+	duration : "Conc, 10 min",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["word or radiance"] = {
+	name : "Word of Radiance",
+	classes : ["cleric"],
+	source : ["X", 171],
+	level : 0,
+	school : "Evoc",
+	time : "1 a",
+	range : "5 ft",
+	components : "V,M",
+	compMaterial : "A holy symbol",
+	duration : "Instantaneous",
+	description : "",
+	descriptionFull : ""
+};
+SpellsList["wrath of nature"] = {
+	name : "Wrath of Nature",
+	classes : ["druid"],
+	source : ["X", 171],
+	level : 5,
+	school : "Evoc",
+	time : "1 a",
+	range : "120 ft",
+	components : "V,S",
+	duration : "Conc, 1 min",
+	description : "",
+	descriptionFull : ""
+};
+
+SourceList["X"] = {
+    name : "Xanathar's Guide to Everything",
+    abbreviation : "XGtE",
+    group : "Primary Sources",
+    url : "http://dnd.wizards.com/products/tabletop-games/rpg-products/xanathars-guide-everything"
+};
