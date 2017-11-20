1482d084ff1c4864ff52d174e6e2d2e150233002
JFSushi <jallefrido@gmail.com>
2017-11-20 16:52:42 +1100

    updated .gitignore

--- XGtE/main.js
+++
@@ -1,9 +0,0 @@
-//Written by JFSushi
-//This file will be updated to contain all scripts for XGtE
-
-SourceList["X"] = {
-    name : "Xanathar's Guide to Everything",
-    abbreviation : "XGtE",
-    group : "Primary Sources",
-    url : "http://dnd.wizards.com/products/tabletop-games/rpg-products/xanathars-guide-everything"
-};
